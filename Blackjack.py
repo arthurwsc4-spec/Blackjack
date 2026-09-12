406d4f5fda339b1c84352f06017bd0764501f907
arthurwsc4-spec <arthurwsc4@gmail.com>
2026-09-11 23:39:25 -0300

    Create Blackjack class with initial methods

---
+++ Blackjack.py
@@ -0,0 +1,16 @@
+class Blackjack():
+    Machine_points = 0
+    def __init__(self):
+        self.points = 0
+
+    def Hit(self):
+        pass
+
+    def Stay(self):
+        pass
+
+    def Restart(self):
+        pass
+
+    def Show_Points(self):
+        pass
