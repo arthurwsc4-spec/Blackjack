28a48120df0843bd986d9a4208345d215c423f2b
arthurwsc4-spec <arthurwsc4@gmail.com>
2026-09-18 09:49:51 -0300

    Update Blackjack.py

--- Blackjack.py
+++ Blackjack.py
@@ -11,7 +11,9 @@ class Blackjack():
     def Build_Deck(self):
         suits = ['Spades', 'Clubs', 'Hearts', 'Diamonds']
         numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-        return [(s, n) for s in suits for n in numbers for _ in range(2)]
+        deck = [(s, n) for s in suits for n in numbers for _ in range(2)]
+        random.shuffle(deck)
+        return deck
 
     def Hit(self, who="player"):
         card = random.choice(self.deck_of_cards)
@@ -23,30 +25,32 @@ class Blackjack():
 
     def Calculate_Hand_Points(self, who="player"):
         total_points = 0
+        aces = 0
+
         if who == "player":
             hand = self.player_hand
         else:
             hand = self.computer_hand
 
         for elem in hand:
-            try:
+            if elem[1] in ['J', 'Q', 'K']:
+                total_points += 10
+            elif elem[1] == 'A':
+                total_points += 11
+                aces += 1
+            else:
                 total_points += int(elem[1])
-            except:
-                if total_points < 11:
-                    total_points += 11
-                elif elem[1] in ['J', 'Q', 'K']:
-                    total_points += 10
-                else:
-                    total_points += 1
+
+        while total_points > 21 and aces > 0:
+            total_points -= 10
+            aces -= 1
+            
         return total_points, who
 
     def Stay(self):
         while self.Calculate_Hand_Points(who="computer")[0] < 18:
             self.Hit(who="computer")
-        if self.Calculate_Hand_Points(who="computer")[0] >= 18:
-            self.Winner()
-        else:
-            self.Hit(who="computer")
+        self.Winner()
 
     def Restart(self):
         self.player_hand = []
@@ -54,7 +58,8 @@ class Blackjack():
         self.deck_of_cards = self.Build_Deck()
 
     def Show_Hand_Points(self):
-        print(f"\nHand points of {self.Calculate_Hand_Points()[1]}: {self.Calculate_Hand_Points()[0]}")
+        points, person = self.Calculate_Hand_Points()
+        print(f"\nHand points of {person}: {points}")
 
     def blackjack(self, who="player"): #this is the only one in lower case so it makes harder to make confusions with the name of the class
         if who == "player":
@@ -63,34 +68,26 @@ class Blackjack():
         else:
             if self.Calculate_Hand_Points(who)[0] == 21:
                 self.computer_points += 1
-
-    def Bust(self, who="player"):
-        if who == "player":
-            points, person = self.Calculate_Hand_Points(who)
-        else:
-            points, person = self.Calculate_Hand_Points(who)
-
-        if points > 21:
-            if person == "player":
-                self.player_points += 1
-            else:
-                self.computer_points += 1
         
     def Winner(self):
         player, _ = self.Calculate_Hand_Points(who="player")
         computer, c = self.Calculate_Hand_Points(who="computer")
         winner_status = False
 
-        if player > 21:
-            self.Bust(who="player")
+        if player > 21 and computer > 21:
+            print('Both players busted, no winners!')
             winner_status = True
+
+        if player > 21:
+            winner_status = True
+            print(f'{red}Player has busted, Computer won!{reset}')
         if player == 21:
             self.blackjack(who="player")
             winner_status = True
 
         if computer > 21:
-            self.Bust(who="computer")
             winner_status = True
+            print(f'{green}Computer has busted, Player has won!{reset}')
         if computer == 21:
             self.blackjack(who="computer")
             winner_status = True
@@ -106,6 +103,7 @@ class Blackjack():
         else:
             print(f"\n\n{green}We got a winner!!{reset}")
             print(f"\nThe score now is: player = {self.player_points} and computer = {self.computer_points}")
+        self.Restart()
 
 #colors for the code
 red = '\033[91m'
